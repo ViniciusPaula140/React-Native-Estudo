bd2964272dc63472d097dbc00ad638af6c3db4ce
Vinícius Paula <viniciuspaularibeiro02@gmail.com>
2024-09-09 20:56:03 -0300

    Correção

--- my-project/App.js
+++ my-project/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Title from './src/components/Title'; //Só deixou eu mudar quando importei a pasta inteira
+import Title from './src/components/Title/';
 
 export default function App() {
   return (
